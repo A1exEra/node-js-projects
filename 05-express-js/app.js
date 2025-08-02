21c6aab63aa6f16b8945ada1f1ca468383422f58
Alex Eramisant <eramisant@yahoo.com>
2025-08-02 17:48:41 +0300

    add ejs to the project

--- 05-express-js/app.js
+++ 05-express-js/app.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const notFound = require("./routes/notFound");
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extanded: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(shopRoutes);
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.router);
 app.use(notFound);
 
 app.listen(3000);
